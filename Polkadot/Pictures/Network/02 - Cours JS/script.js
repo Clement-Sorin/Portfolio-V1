360a75e5ccce4e3c85ff495cdbba0bedc5e02270
Clément Sorin <clement.sorin.141@gmail.com>
2024-07-26 17:26:16 +0200

    initial commit

---
+++ Polkadot/Pictures/Network/02 - Cours JS/script.js
@@ -0,0 +1,15 @@
+var age = 15;
+
+
+if(age<30)
+{
+    console.log("Quel beau gosse!");
+}
+else if(age<46.5)
+{
+    console.log("Tu es mûr et viril, hummmm");
+}
+else
+{
+    console.log("Tu es vieux est impuissant!");
+}
